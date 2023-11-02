9cb941e280c7b4e8d889f4edd30ffe2eb7a9358b
DFoster-2 <138312355+DFoster-2@users.noreply.github.com>
2023-11-02 17:31:55 +0000

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,19 @@
+print("Function")
+
+function add(x, y)
+  print(x, "+", y, "is", x+y)
+end
+
+x = read("Hello ")
+y = read("Hi ")
+add(x, y)
+
+variable = "hi"
+
+print(variable)
+
+x = 5
+
+if (x == 5) then
+  print(x)
+end
